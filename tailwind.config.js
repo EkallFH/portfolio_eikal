51d2ed55b03a3e6e44bdb51459790840478247f5
EkallFH <mhmmdfkrihaikall@gmail.com>
2024-07-08 11:07:00 +0700

    Portfolio Fikri Haikal

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#00004d',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        'xl': '1320px',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
